04f05f43b8e022297822d4b2f82d74bab48c5281
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2021-07-01 15:36:19 +0200

    cleaning up

--- include/tiago_controller/joint_controller.hpp
+++ include/tiago_controller/joint_controller.hpp
@@ -1,9 +1,8 @@
-#ifndef JointController_HPP_
-#define JointController_HPP_
+#ifndef TIAGO_JointController_HPP_
+#define TIAGO_JointController_HPP_
 
-#include "std_msgs/Float64MultiArray.h"
-#include <robot_framework_msgs/command.h>
 #include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
 
 #include <eigen3/Eigen/Core>
 
@@ -14,7 +13,6 @@
 #include <boost/bind.hpp>
 #include <chrono>
 #include <iostream>
-#include <kdl_parser/kdl_parser.hpp>
 #include <map>
 #include <memory>
 #include <string>
@@ -27,7 +25,6 @@ namespace tiago_controller {
         JointController() {}
         ~JointController() {}
         void update(const ros::Time& time, const ros::Duration& period);
-        void updateCommand(const robot_framework_msgs::command::ConstPtr& msg);
 
         bool initRequest(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh,
             ros::NodeHandle& controller_nh, ClaimedResources& claimed_resources) override;
@@ -40,13 +37,6 @@ namespace tiago_controller {
         bool init(hardware_interface::PositionJointInterface* position_iface,
             hardware_interface::VelocityJointInterface* velocity_iface,
             ros::NodeHandle& root_nh, ros::NodeHandle& control_nh);
-        std::vector<hardware_interface::JointHandle> joints_;
-        std::vector<std::string> jNames_;
-        std::map<std::string, double> mapQdesired_, mapQInit_;
-        std::shared_ptr<ros::Subscriber> joint_command_sub_;
-        double cont;
-        std::chrono::high_resolution_clock::time_point last_t_;
-        double dt_ = 0.001;
     };
 } // namespace tiago_controller
 
